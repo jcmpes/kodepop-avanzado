b7667d30493c14311336d2b69ffe480b6c832833
jc <jcmpes@gmail.com>
2021-02-01 10:24:54 +0100

    Added description field to the Ad model

--- models/Ad.js
+++ models/Ad.js
@@ -2,10 +2,18 @@
 
 const mongoose = require('mongoose');
 
+// Define schema
 const adSchema = mongoose.Schema({
-    title: { type: String, index: true },
+    title: { type: String },
+    description: String,
     price: Number,
     type: String,
     img: String,
     tags: [String]
-})
+});
+
+// Create model object, specify collection and schema
+const Ad = mongoose.model('ad', adSchema);
+
+// Export model
+module.exports = Ad;
