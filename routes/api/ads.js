a9672c43d554ecc3ed2c334cdf75a09375379e77
jc <jcmpes@gmail.com>
2021-02-04 12:21:24 +0100

    Added skip and limit to route ads

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -4,16 +4,29 @@ var router = express.Router();
 const Ad = require('../../models/Ad');
 
 /**
- * GET /api/ads
  * Lista de agentes
+ * GET /api/ads
+ * 
+ * Lista de agentes con paginación
+ * GET /api/ads?skip=10&limit=10
  */
 router.get('/', async function(req, res, next) {
     try {
-        const result = await Ad.find();
+        const filter = {};
+        if (type) {
+            filter.type = type;
+        }
+        const limit = parseInt(req.query.limit);
+        const skip = parseInt(req.query.skip);
+        const result = await Ad.filterAndList(filter, limit, skip);
         res.json(result);
     } catch (err) {
         next(err);
     }
 });
+
+/**
+ * 
+ */
 
 module.exports = router;
