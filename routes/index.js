aacb313f43fe34a601040fa308e77cc081bb0c47
jc <jcmpes@gmail.com>
2021-02-07 14:10:31 +0100

    Added router /filters to filter in the ejs view by price

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,28 @@ router.get('/', async function(req, res, next) {
       return 
     }
     res.render('index', { title: 'Keepads', ads: result });
-
   })
+});
+
+/**
+ * GET ads filtered by price in the ejs view 
+ * GET /filters?lt=100&gt=40
+ */
+router.get('/filters', async function(req, res, next) {
+    const max = req.query.lt;
+    const min = req.query.gt;
+
+    await Ad
+            .find({ 
+                    price: { $lt: max, $gt: min },
+
+                  })
+            .then(result => {
+              res.render('index', { title: 'Keepads', ads: result});
+            })
+            .catch(err => {
+              next(err)
+            });
 });
 
 module.exports = router;
